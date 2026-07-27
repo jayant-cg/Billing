1960710d50e74736ed6621269b4cb517752dd6bd
Jayant <jayant.mishra@cginfinity.com>
2026-07-27 11:29:41 +0530

    deployment

--- FRONTEND/vite.config.js
+++ FRONTEND/vite.config.js
@@ -5,8 +5,11 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      '/api': 'https://billingcg-bbgybfaafkdda0g2.indiasouthcentral-01.azurewebsites.net' //
-
+      '/api': {
+        target: 'https://billingcg-bbgybfaafkdda0g2.indiasouthcentral-01.azurewebsites.net',
+        changeOrigin: true,
+        secure: true
+      }
     }
   }
 })
